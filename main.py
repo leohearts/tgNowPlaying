0995b8707998e1df75ae43c0ab0615463df6d176
Leohearts <leohearts@leohearts.com>
2024-04-16 23:46:51 +0800

    impl emby

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from settings import api_id, api_hash
 
 # The first parameter is the .session file name (absolute paths allowed)
 client = TelegramClient('leohearts', api_id, api_hash)
-
+full = await client(GetFullUserRequest('me'))
+bio = full.full_user.about
+print(bio)
 app = Flask(__name__)
 
 @app.route('/<path:path>',methods=['POST'])
@@ -14,7 +16,27 @@ def handler(path):
     if request.method=='POST':
         req = request.get_json()
         print(req)
-        response = Response('{"msg": "ok"}', 500)
+        response = Response('{"msg": "No source"}', 500)
+        if "emby" in path:
+            if req["Event"] == "playback.start":
+                try:
+                    title = req["Title"].split("开始播放 ")[1]
+                    client(UpdateProfileRequest(
+                        about='▶️Playing ' + title
+                    ))
+                    response = Response({"msg": "Updated to %s" % title} , 200)
+                except Exception as e:
+                    print(e)
+                    pass
+            else if req["Event"] == "playback.stop":
+                try:
+                    client(UpdateProfileRequest(
+                        about=bio
+                    ))
+                    response = Response({"msg": "Updated to %s" % bio } , 200)
+                except Exception as e:
+                    print(e)
+                    pass
         return response
     else:
         response = Response('{"msg": "Method not allowed"}', 500)
