2ea7ea9204015723f939a2d11920df92b33842d3
Leohearts <leohearts@leohearts.com>
2024-04-16 23:56:02 +0800

    fix init

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from telethon.tl.functions.account import UpdateProfileRequest
+from telethon.tl.functions.users import GetFullUserRequest
 from telethon import TelegramClient
 from flask import Flask,request,redirect,Response
-
+import asyncio
 from settings import api_id, api_hash
 
 # The first parameter is the .session file name (absolute paths allowed)
+bio = ""
 client = TelegramClient('leohearts', api_id, api_hash)
-full = await client(GetFullUserRequest('me'))
-bio = full.full_user.about
-print(bio)
+async def init():
+    async with client:
+        full = await client(GetFullUserRequest('me'))
+        bio = full.full_user.about
+        print(bio)
+
+asyncio.run(init())
 app = Flask(__name__)
 
+
 @app.route('/<path:path>',methods=['POST'])
-def handler(path):
+async def handler(path):
     if request.method=='POST':
         req = request.get_json()
         print(req)
@@ -21,18 +28,20 @@ def handler(path):
             if req["Event"] == "playback.start":
                 try:
                     title = req["Title"].split("开始播放 ")[1]
-                    client(UpdateProfileRequest(
-                        about='▶️Playing ' + title
-                    ))
+                    async with client:
+                        await client(UpdateProfileRequest(
+                            about='▶️Playing ' + title
+                        ))
                     response = Response({"msg": "Updated to %s" % title} , 200)
                 except Exception as e:
                     print(e)
                     pass
             elif req["Event"] == "playback.stop":
                 try:
-                    client(UpdateProfileRequest(
-                        about=bio
-                    ))
+                    async with client:
+                        await client(UpdateProfileRequest(
+                            about=bio
+                        ))
                     response = Response({"msg": "Updated to %s" % bio } , 200)
                 except Exception as e:
                     print(e)
